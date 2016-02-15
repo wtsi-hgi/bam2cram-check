a6e8a6d60f5c38c6be30ba4f1aa7ae7df1999097
IrinaColgiu <ic4@sanger.ac.uk>
2016-02-15 22:21:08 +0000

    Refactored further the methods in stats_checks

--- checks/stats_checks.py
+++ checks/stats_checks.py
@@ -33,43 +33,52 @@ class RunSamtoolsCommands:
 class HandleSamtoolsStats:
 
     @classmethod
-    def get_stats(cls, stats_fpath):
+    def _get_stats(cls, stats_fpath):
         if stats_fpath and os.path.isfile(stats_fpath):
             return utils.read_from_file(stats_fpath)
         return None
         #raise ValueError("No stats file found: %s" % str(stats_fpath))
 
     @classmethod
-    def generate_stats(cls, data_fpath):
+    def _generate_stats(cls, data_fpath):
         if not data_fpath or not os.path.isfile(data_fpath):
             raise ValueError("Can't generate stats from a non-existing file: %s" % str(data_fpath))
         return RunSamtoolsCommands.get_samtools_stats_output(data_fpath)
 
     @classmethod
-    def save_stats(cls, stats, stats_fpath):    # =data_fpath+'.stats'
+    def _save_stats(cls, stats, stats_fpath):    # =data_fpath+'.stats'
         if not stats or not stats_fpath:
             raise ValueError("You must provide both stats and stats file path for saving the stats to a file."
                          " Received stats = %s and stats fpath = %s" % (str(stats), str(stats_fpath)))
         return utils.write_to_file(stats_fpath, stats)
 
     @classmethod
-    def create_and_save_stats(cls, stats_fpath, data_fpath, ):
-        stats = RunSamtoolsCommands.get_samtools_stats_output(data_fpath)
-        utils.write_to_file(stats_fpath, stats)
+    def fetch_and_persist_stats(cls, fpath):
+        stats_fpath = fpath + ".stats"
+        stats = HandleSamtoolsStats._get_stats(stats_fpath) # TODO: try - catch ValueError
+        if not stats:
+            stats = HandleSamtoolsStats._generate_stats(fpath)
+            HandleSamtoolsStats._save_stats(stats, stats_fpath)
         return stats
 
-    @classmethod
-    def get_or_create_stats(cls, stats_fpath, data_fpath=None):
-        if not stats_fpath and not data_fpath:
-            raise ValueError("Insufficient information, the parameters stats_file and data_file can't both be None.")
-        if stats_fpath:
-            if not os.path.isfile(stats_fpath):
-                raise ValueError("Stats filepath is not a valid path: %s" % stats_fpath)
-            return utils.read_from_file(stats_fpath)
-        elif data_fpath:
-            if not os.path.isfile(data_fpath):
-                raise ValueError("Data filepath is not a valid path: %s" % data_fpath)
-            return cls.create_and_save_stats(stats_fpath, data_fpath)
+    # @classmethod
+    # def create_and_save_stats(cls, stats_fpath, data_fpath, ):
+    #     stats = RunSamtoolsCommands.get_samtools_stats_output(data_fpath)
+    #     utils.write_to_file(stats_fpath, stats)
+    #     return stats
+    #
+    # @classmethod
+    # def get_or_create_stats(cls, stats_fpath, data_fpath=None):
+    #     if not stats_fpath and not data_fpath:
+    #         raise ValueError("Insufficient information, the parameters stats_file and data_file can't both be None.")
+    #     if stats_fpath:
+    #         if not os.path.isfile(stats_fpath):
+    #             raise ValueError("Stats filepath is not a valid path: %s" % stats_fpath)
+    #         return utils.read_from_file(stats_fpath)
+    #     elif data_fpath:
+    #         if not os.path.isfile(data_fpath):
+    #             raise ValueError("Data filepath is not a valid path: %s" % data_fpath)
+    #         return cls.create_and_save_stats(stats_fpath, data_fpath)
 
     @classmethod
     def extract_checksum_from_stats(cls, stats: str) -> str:
@@ -81,54 +90,53 @@ class HandleSamtoolsStats:
 
 class CompareStatsForFiles:
     @classmethod
-    def compare_files_by_flagstats(cls, bam_path, cram_path):
+    def compare_files_by_flagstats(cls, flagstat_b, flagstat_c):
         errors = []
-        flagstat_b = RunSamtoolsCommands.get_samtools_flagstat_output(bam_path)
-        flagstat_c = RunSamtoolsCommands.get_samtools_flagstat_output(cram_path)
         if flagstat_b != flagstat_c:
-            logging.error("FLAGSTAT DIFFERENT for %s : %s and %s : %s " % (bam_path, flagstat_b, cram_path, flagstat_c))
-            errors.append("FLAGSTAT DIFFERENT for %s : %s and %s : %s " % (bam_path, flagstat_b, cram_path, flagstat_c))
+            logging.error("FLAGSTAT DIFFERENT:\n %s then:\n %s " % (flagstat_b, flagstat_c))
+            errors.append("FLAGSTAT DIFFERENT:\n %s then:\n %s " % (flagstat_b, flagstat_c))
         return errors
 
     @classmethod
-    def compare_files_by_sequence_checksum(cls, bam_path, cram_path):
+    def compare_files_by_sequence_checksum(cls, stats_b, stats_c):
         errors = []
-        stats_path_b = bam_path + ".stats"
-        stats_path_c = cram_path + ".stats"
-
-        stats_b = HandleSamtoolsStats.get_stats(stats_path_b)
-        if not stats_b:
-            stats_b = HandleSamtoolsStats.generate_stats(bam_path)
-            HandleSamtoolsStats.save_stats(stats_b, stats_path_b)
-
-        stats_c = HandleSamtoolsStats.get_stats(stats_path_c)
-        if not stats_c:
-            stats_c = HandleSamtoolsStats.generate_stats(cram_path)
-            HandleSamtoolsStats.save_stats(stats_c, stats_path_c)
+        # stats_path_b = bam_path + ".stats"
+        #
+        #
+        # stats_c = HandleSamtoolsStats._get_stats(stats_path_c)
+        # if not stats_c:
+        #     stats_c = HandleSamtoolsStats._generate_stats(cram_path)
+        #     HandleSamtoolsStats._save_stats(stats_c, stats_path_c)
 
         # stats_b = HandleSamtoolsStats.get_or_create_stats(stats_path_b, bam_path)
         # stats_c = HandleSamtoolsStats.get_or_create_stats(stats_path_c, cram_path)
+
 
         chk_b = HandleSamtoolsStats.extract_checksum_from_stats(stats_b)
         chk_c = HandleSamtoolsStats.extract_checksum_from_stats(stats_c)
 
         if not chk_b:
-            errors.append(("For some reason there is no CHK line in the samtools stats of %s " % bam_path))
-            logging.error("For some reason there is no CHK line in the samtools stats of %s " % bam_path)
+            errors.append("For some reason there is no CHK line in the samtools stats")
+            logging.error("For some reason there is no CHK line in the samtools stats")
         if not chk_c:
-            errors.append(("For some reason there is no CHK line in the samtools stats of %s " % cram_path))
-            logging.error("For some reason there is no CHK line in the samtools stats of %s " % cram_path)
+            errors.append("For some reason there is no CHK line in the samtools stats")
+            logging.error("For some reason there is no CHK line in the samtools stats")
 
         if chk_b != chk_c:
-            errors.append("STATS SEQUENCE CHECKSUM DIFFERENT for %s: %s and %s: %s" % (bam_path, chk_b, cram_path, chk_c))
-            logging.error("STATS SEQUENCE CHECKSUM DIFFERENT for %s: %s and %s: %s" % (bam_path, chk_b, cram_path, chk_c))
+            errors.append("STATS SEQUENCE CHECKSUM DIFFERENT: %s and %s" % (chk_b, chk_c))
+            logging.error("STATS SEQUENCE CHECKSUM DIFFERENT: %s and %s" % (chk_b, chk_c))
         return errors
 
     @classmethod
     def compare_bam_and_cram_by_statistics(cls, bam_path, cram_path):
         errors = []
-        errors.extend(cls.compare_files_by_flagstats(bam_path, cram_path))
-        errors.extend(cls.compare_files_by_sequence_checksum(bam_path, cram_path))
+        flagstat_b = RunSamtoolsCommands.get_samtools_flagstat_output(bam_path)
+        flagstat_c = RunSamtoolsCommands.get_samtools_flagstat_output(cram_path)
+        errors.extend(cls.compare_files_by_flagstats(flagstat_b, flagstat_c))
+
+        stats_b = HandleSamtoolsStats.fetch_and_persist_stats(bam_path)
+        stats_c = HandleSamtoolsStats.fetch_and_persist_stats(cram_path)
+        errors.extend(cls.compare_files_by_sequence_checksum(stats_b, stats_c))
         return errors
 
 
